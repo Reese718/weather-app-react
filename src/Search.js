915e0bcf411cb15cf6d906f62fc235019bde8c1c
ReeseS <ReeseS718@users.noreply.github.com>
2021-04-01 21:24:09 -0400

    remove duplicated className attributes

--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,7 @@ import "./Search.css";
 
 export default function Search() {
   return (
-    <div className="weatherBody" className="Search">
+    <div className="Search">
       <h1 className="city-name">New York</h1>
 
       <form className="search-form">
@@ -11,11 +11,9 @@ export default function Search() {
           <i className="fas fa-map-marker-alt"></i>
         </button>
         <input
-          className="col-8"
           type="text"
           placeholder="Enter City"
           className="enter-city"
-          className="search-city"
           autofocus="on"
           autocomplete="off"
         />
